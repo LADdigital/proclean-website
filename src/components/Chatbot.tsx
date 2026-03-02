81e780e43c4f660b2d3c1061dabbbcbb84ec9f3b
LADdigital <jndvillalobos0521@gmail.com>
2026-03-02 15:14:44 -0800

    Updated Chatbot.tsx

--- src/components/Chatbot.tsx
+++ src/components/Chatbot.tsx
@@ -337,7 +337,7 @@ export default function Chatbot({ isOpen, setIsOpen }: ChatbotProps) {
               <defs>
                 <path
                   id="topArc"
-                  d="M 22,44 A 40,40 0 0,1 102,44"
+                  d="M 22,49 A 40,40 0 0,1 102,49"
                   fill="none"
                 />
               </defs>
